385c386a3da1471c12ce21bd770a39684ec6a22e
samserious125 <samserious125@gmail.com>
2022-12-12 18:56:09 +0300

    Add second console

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace CastleProject
         static void Main(string[] args)
         {
             Console.WriteLine("It is my castle!");
+            Console.WriteLine("It is my castle 2!");
         }
     }
 }
